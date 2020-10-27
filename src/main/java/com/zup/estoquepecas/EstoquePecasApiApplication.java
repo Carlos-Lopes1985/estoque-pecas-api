541ebf500570da800417620edb27db06ff50457c
Carlos-Lopes1985 <carlosslopes1985@hotmail.com>
2020-10-27 00:04:11 -0300

    prieiro commit

---
+++ src/main/java/com/zup/estoquepecas/EstoquePecasApiApplication.java
@@ -0,0 +1,13 @@
+package com.zup.estoquepecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoquePecasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoquePecasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zup/estoquepecas/EstoquePecasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.estoquepecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoquePecasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
